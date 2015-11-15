538e57a81a9a591ecdb863677cd01313dd7f4c1c
Sequoia Snow <the1codemaster@gmail.com>
2015-11-15 11:31:35 -0800

    Second commit

--- lib/snowbutton.rb
+++ lib/snowbutton.rb
@@ -2,7 +2,7 @@ require "snowbutton/version"
 require "compass"
 
 base_directory = File.join(File.dirname(__FILE__))
-Compass::Frameworks.register('pixie_sass', :path => base_directory)
+Compass::Frameworks.register('snowbutton', :path => base_directory)
 
 module Snowbutton
   # Your code goes here...
